90046c9a78108614f6981ca22e0c3da0199662e9
Ayushkar Roy <64706406+ayushkar@users.noreply.github.com>
2021-11-24 03:37:55 +0530

    Update formprocess.php

--- formprocess.php
+++ formprocess.php
@@ -1,7 +1,7 @@
 <?php
 include("config.php");
 extract($_POST);
-$query = "INSERT INTO `contactdata`(`destination`, `	email`, `arrivals`, `departure`) VALUES ('".$destination."','".$email."',".$arrivals.",'".$departure."')";
+$query = "INSERT INTO `contactdata`(`destination`, `email`, `arrivals`, `departure`) VALUES ('".$destination."','".$email."',".$arrivals.",'".$departure."')";
 $result = $mysqli->query($query);
 if(!$result){
     die("Couldn't enter data: ".$mysqli->error);
