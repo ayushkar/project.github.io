5af297dc4239a7356b2d6a911782d060f13a1134
Ayushkar Roy <64706406+ayushkar@users.noreply.github.com>
2021-11-24 03:38:28 +0530

    Update config.php

--- config.php
+++ config.php
@@ -2,6 +2,6 @@
 define("DB_HOST","localhost");
 define("DB_USER","root");
 define("DB_PASSWORD","");
-define("DB_NAME","contactdata");
+define("DB_NAME","contact-data");
 $mysqli = new mysqli(DB_HOST,DB_USER, DB_PASSWORD,DB_NAME);
 ?>
